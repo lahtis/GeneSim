e65cc44d88cbc49290e87e034ff63760706856c9
Tuomas Lähteenmäki <lahtis@users.noreply.github.com>
2025-12-20 21:53:56 +0200

    Add files via upload

--- namegen/include/loader.h
+++ namegen/include/loader.h
@@ -2,27 +2,28 @@
 #define LOADER_H
 
 typedef struct {
+    char *first;
+    char *second;
+    char *last;
+} Name;
+
+typedef struct {
     char *firstMDataPaths;
     char *secondMDataPaths;
     char *firstFDataPaths;
     char *secondFDataPaths;
     char *lastDataPaths;
     char *output_file;
-    char *format;
     int count;
+    char *format;
     int verbose;
 } Config;
 
-typedef struct {
-    char *first;
-    char *second;
-    char *last;
-} Name;
+
 
 Config *load_config(const char *filename);
-void free_config(Config *cfg);
 
-Name *load_names(const char *filename, int *count);
+Name *load_names(const char *filename, int target_period, int *count);
 void free_names(Name *names, int count);
-
+void free_config(Config *cfg);
 #endif
