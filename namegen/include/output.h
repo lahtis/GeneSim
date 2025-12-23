619f4bfa3a50cc8c5e208ea9c27658addbafc651
Tuomas Lähteenmäki <lahtis@users.noreply.github.com>
2025-12-23 02:05:15 +0200

    bug fixed
    bug fixed

--- namegen/include/output.h
+++ namegen/include/output.h
@@ -3,6 +3,9 @@
 
 #include "loader.h"
 #include <stdio.h>
+#define OUTPUT_PLAIN 0
+#define OUTPUT_CSV 1
+#define OUTPUT_JSON 2
 
 void write_formatted_name_full(FILE *fp,
                             const char *format,
@@ -26,5 +29,9 @@ void print_file(FILE *fp,
                 const Name *last,
                 int birth_year,
                 int verbose);
+
+void write_formatted_name_full(FILE *fp, const char *format, const Name *f_ptr, const Name *s_ptr, const Name *l_ptr, const Name *m_ptr, int birth_year);
+void print_name(const char *format, const Name *first, const Name *second, const Name *last, int birth_year, int verbose);
+void print_file(FILE *fp, const char *format, const Name *first, const Name *second, const Name *last, int birth_year, int verbose);
 
 #endif /* OUTPUT_H */
